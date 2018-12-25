3c984e23153406715e70952684a93654400aaf3c
lenovo <lenovo@DESKTOP-1ILDOSM>
2018-12-25 05:12:55 -0800

    Code is commited

---
+++ src/SeleniumWeb/Test1.java
@@ -0,0 +1,29 @@
+package SeleniumWeb;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Viraj");
+		al.add("Santhi");
+		al.add("Rajesh");
+		al.add("Siri");
+		al.add("Sri");
+		al.add("Prameela");
+		Collections.sort(al);
+   Iterator itr = al.iterator();
+	while(itr.hasNext()){	
+		   
+	System.out.println(itr.next());	 
+		
+	
+	
+	}
+
+}
+}
